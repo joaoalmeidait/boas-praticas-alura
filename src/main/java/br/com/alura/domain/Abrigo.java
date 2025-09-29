ad7138322a023d282d7c508648efe238f91f640e
joaoalmeida <joaoalmeidapedrosa@gmail.com>
2025-09-29 20:17:06 -0300

    ajustando metodos para usar jackson-databind

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,9 +2,11 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
 
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
@@ -12,5 +14,26 @@ public class Abrigo {
         this.email = email;
     }
 
+    public Abrigo() {
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
